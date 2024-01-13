b3c4ed8ffb2087af0c5cb4f56c37aff41ec04405
Emrockno <calebsaltandsugar@gmail.com>
2024-01-12 23:30:54 -0600

    first test

---
+++ src/main/java/emrockno/aidenisgay/Keybinding.java
@@ -0,0 +1,9 @@
+package emrockno.aidenisgay;
+
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+
+public class Keybinding {
+
+}
