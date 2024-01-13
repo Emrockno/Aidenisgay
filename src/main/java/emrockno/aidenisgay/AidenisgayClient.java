13c634652c63e180ef5eb5b9bc3eb863db076617
Emrockno <calebsaltandsugar@gmail.com>
2024-01-12 23:05:58 -0600

    first test

---
+++ src/main/java/emrockno/aidenisgay/AidenisgayClient.java
@@ -0,0 +1,10 @@
+package emrockno.aidenisgay;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AidenisgayClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/emrockno/aidenisgay/AidenisgayDataGenerator.java
@@ -0,0 +1,11 @@
+package emrockno.aidenisgay;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AidenisgayDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
